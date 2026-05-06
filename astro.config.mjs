a178a471e3888b0f47b8c1a7a423f81f55a0ed19
jorgefernandez88 <jorgefernandez67.11@gmail.com>
2026-05-05 19:10:16 -0600

    fix: se realizan labores de mantenimiento, se cambia a la version 5.18.1 de Astro y una modificacion en el astro.config para deployar el portal en cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,5 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-    site: 'https://jorgefernandez88.github.io',
-    base: '/PortalinksU/',
+  output: 'static'
 });
